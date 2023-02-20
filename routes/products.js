1c566006fa82a5f1b24f7289f1c4f9093f9401a4
KJaybhaye <krushnajaybhaye01@gmail.com>
2023-02-20 15:02:39 +0530

    some changes to routing

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,10 @@
-const {create, getAll, getOne, update, deleteProduct, addComment, addRating} = require("../controllers/products");
+const {create, getAll, getOne, update, deleteProduct, addReview, deleteReview, updateReview} = require("../controllers/products");
 const express = require("express");
 
 const router = express.Router();
 
 router.route("/").get(getAll).post(create);
 router.route("/:id").get(getOne).patch(update).delete(deleteProduct);
-router.route("/:id/add-comment").patch(addComment);
-router.route("/:id/add-rating").patch(addRating);
+router.route("/:id/review").post(addReview).delete(deleteReview).patch(updateReview);
 
 module.exports = router;
