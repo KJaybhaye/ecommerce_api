bfb1fd8b8c92ec96b0c7b35ec2288cf5799b7b74
KJaybhaye <krushnajaybhaye01@gmail.com>
2023-02-20 15:05:24 +0530

    some changes to routing

--- routes/account.js
+++ routes/account.js
@@ -3,9 +3,6 @@ const {addToCart, addToWish, deleteFromCart, deleteFromWish} = require("../contr
 
 const router = express.Router();
 
-// router.route("/address").post(addAddresss).patch(updateAddress).delete(deleteAddresss);
-// router.route("/wishlist").post(addToWish);
-// router.route("/cart").post(addToCart);
 router.route("/wishlist/:id").post(addToWish).delete(deleteFromWish);
 router.route("/cart/:id").post(addToCart).delete(deleteFromCart);
 module.exports = router;
