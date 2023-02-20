a4be29e3bfb93b12d7e2128094224021be0587aa
KJaybhaye <krushnajaybhaye01@gmail.com>
2023-02-20 14:58:31 +0530

    added admin registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,6 +7,12 @@ const register = async (req, res, next) => {
     // if(!name || !email || !password){
     //     throw new BadRequest("provid all values (name, email, password");
     // }
+    if(req.body.userType === "admin"){
+        if(!req.body.adminPassword || req.body.adminPassword != process.env.ADMIN_PASSWOWRD){
+            const err = new BadRequest("Adming Password is wrong.")
+            return next(err);
+        }
+    }
     try {
         const newUser = await User.create({...req.body});
         res.status(200).json({user: {name: newUser.getName()}, token:newUser.getJWT()});
@@ -18,15 +24,18 @@ const register = async (req, res, next) => {
 const login = async (req, res, next) => {
     const {email, password} = req.body;
     if(!email  || !password){
-        throw new BadRequest("provid all values (email, password");
+        const err = new BadRequest("Provid all values (email, password");
+        return next(err);
     }
     const user = await User.findOne({email: email});
     if(!user){
-        throw new AuthError("invalid credentails");
+        const err = new AuthError("Invalid credentails");
+        return next(err);
     }
     const correct = user.comparePass(password);
     if(!correct){
-        throw new AuthError("invalid credentails");
+        const err = new AuthError("Invalid credentails");
+        return next(err);
     }
     res.status(200).json({user: {name: user.getName()}, token:user.getJWT()});
 }
