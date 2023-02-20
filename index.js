01ec9938794bac5d572df460e3fc3c00984c551d
KJaybhaye <krushnajaybhaye01@gmail.com>
2023-02-20 16:12:39 +0530

    added security features

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const errorHandler = require("./middlewares/errorHandler");
 const authMiddleware = require("./middlewares/auth");
 
 //security
+const helmet = require("helmet");
 const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
 
 
 // utils
@@ -24,6 +27,18 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+//security
+app.set("trust proxy", 1)
+app.use( rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // app use
 app.use(express.json());
